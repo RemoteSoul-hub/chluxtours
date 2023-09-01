78c66106596f99b9f61cfe71a3dd09296fb923cd
Yahya Tahoune <74861627+RemoteSoul-hub@users.noreply.github.com>
2023-09-01 19:06:55 +0100

    Initial setup REQUIRED

--- sanity-studio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'isca4gdi',
-    dataset: 'production'
-  }
-})
--- sanity-studio/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import car from './car'
-import user from './user'
-import reservation from './reservation'
-
-export const schemaTypes = [car, user, reservation]
